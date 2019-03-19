6207691a5eb145a6793ea53f94abda4355c5bc0b
Wizard-C <44673127+Wizard-C@users.noreply.github.com>
2019-03-19 16:59:37 +0500

    mast
    final_test

--- верстка КИА/js/slider.js
+++ верстка КИА/js/slider.js
@@ -1,1 +1,33 @@
-
+window.addEventListener("DOMContentLoaded", function() {
+    var left = document.querySelector(".slider__left"),
+        right = document.querySelector(".slider__right"),
+        container = document.querySelector(".slider__container"),
+        timer;
+    right.addEventListener("click", function() {
+        window.clearTimeout(timer);
+        var last = container.querySelector(".slider__item:last-child");
+        var first = container.querySelector(".slider__item:first-child");
+        container.classList.remove("animate");
+        if (!container.classList.contains("left")) {
+            container.classList.add("left");
+            container.insertBefore(last, first);
+        }
+        timer = window.setTimeout(function() {
+            container.classList.add("animate");
+            container.classList.remove("left")
+        },30)
+    });
+    left.addEventListener("click", function() {
+        window.clearTimeout(timer);
+        var first = container.querySelector(".slider__item:first-child");
+        container.classList.remove("animate");
+        if (container.classList.contains("left")) {
+            container.classList.remove("left");
+            container.appendChild(first)
+        }
+        timer = window.setTimeout(function() {
+            container.classList.add("animate");
+            container.classList.add("left")
+        },30)
+    })
+});
