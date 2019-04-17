b1d4b674ff4b7a52e085ed694c5d541263452d68
Wizard-C <44673127+Wizard-C@users.noreply.github.com>
2019-04-17 16:16:32 +0500

    начало логики

--- KIA/KIA/main/models.py
+++ KIA/KIA/main/models.py
@@ -1,3 +1,24 @@
+# Логика такая, мы парсим все в базу данных.
+# И достаем из нее на хтмл страницу в формате орм
+
 from django.db import models
 
 # Create your models here.
+
+class MyModelName(models.Model):
+    car_name = models.CharField(max_length=255, help_text="")
+    discount = models.CharField(max_length=20, help_text="Enter field documentation")
+    quantity =
+    year =
+    engine =
+    component =
+    color =
+    car_img =
+
+
+    # Metadata
+    class Meta:
+        ordering = ["-my_field_name"]
+
+    def __str__(self):
+        return self.field_name
